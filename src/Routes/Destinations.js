ff4b4effb13635a7738f035cc66611760b2862bd
IulianC <citan.iulian@yahoo.com>
2024-02-18 16:08:25 +0200

    src

---
+++ src/Routes/Destinations.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Destinations ()  {
+    return (
+        
+        <div className="destinations">
+            <h1>Destinations</h1>
+        </div>
+    )
+    
+} 
+
+ export default Destinations;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+import Blog from "./Routes/Blog";
+import Home from "./Routes/Home";
+import Destinations from "./Routes/Destinations";
+import AboutUs from "./Routes/About Us";
+import Navbar from "./Components/Navbar";
+import "./App.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const AppLayout = () => (
+    <>
+    <Navbar />
+    <Outlet />
+    </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router = createBrowserRouter([
+    {
+    element: <AppLayout/>,
+   children:[
+    {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "Blog",
+        element: <Blog />,
+      },
+      {
+        path: "Destinations",
+        element: <Destinations />,
+      },
+      {
+        path: "AboutUs",
+        element: <AboutUs />,
+      },
+
+   ],
+    },
+ 
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
